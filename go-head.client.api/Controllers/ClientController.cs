71386453c3276578957abf0857b97009476862e1
mauricionofre <mauricionofre@hotmail.com>
2021-10-29 19:03:48 -0300

    Init

---
+++ go-head.client.api/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace go_head.client.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new List<string>
+            {
+                "Jaba",
+                "Onofre",
+                "Vivi",
+            };
+        }
+    }
+}
